65a88287bc07f167c78f866b808784ae4a42ea98
youngleek0918 <younglee0918@gmail.com>
2020-07-08 12:43:29 +0900

    hash password

--- modules/User.js
+++ modules/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +33,25 @@ const userSchema = mongoose.Schema({
     },
 })
 
-const User = mongoose.model('User', userSchema)  
+userSchema.pre('save', function( next ) {
+    var user = this;
+    
+    if(user.isModified('password')){    
+        // console.log('password changed')
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
