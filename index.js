a2bd1bf556e360278e6a2c367423253dfcaca944
youngleek0918 <younglee0918@gmail.com>
2020-07-08 14:43:18 +0900

    login function with jsonwebtoken

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 5000
-const bodyParser = require('body-parser')
-const { User } = require("./modules/User")
-const config = require('./config/key')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { User } = require("./modules/User");
 
-app.use(bodyParser.urlencoded({extended: true}));
+//application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//application/json 
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -28,4 +30,32 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
+app.post('/login', (req, res) => {
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+  
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "Not founded"
+        })
+      }
+  
+      user.comparePassword(req.body.password, (err, isMatch) => {
+
+        if (!isMatch)
+          return res.json({ loginSuccess: false, message: "Incorrect password" })
+  
+        user.generateToken((err, user) => {
+          if (err) return res.status(400).send(err);
+  
+          res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+        })
+      })
+    })
+  })
+  
+  const port = 5000
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
